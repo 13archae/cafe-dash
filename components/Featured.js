c33373e78ba07c16579740c6f5f31644918c6ff8
archae13 <archae13@lpbroadband,net>
2024-09-27 16:39:01 -0600

    Added Card Component to Featured

--- components/Featured.js
+++ components/Featured.js
@@ -1,20 +1,42 @@
 import Image from "next/image";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+} from "reactstrap";
 import styles from "../styles/Footer.module.css";
 
 const Featured = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="/img/OysterBar.png"
-            alt="featured restaurant"
-            width="800"
-            height="400"
-          />
-        </div>
-      </div>
-    </div>
+    <>
+      <h1>Featured Cafe</h1>
+      <Card
+        style={{
+          width: "24rem",
+        }}
+      >
+        <Image
+          src="/img/OysterBar.png"
+          alt="featured restaurant"
+          width="400"
+          height="200"
+        />
+        <CardBody>
+          <CardTitle tag="h5">The Cosmic Clock Oyster House</CardTitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            The best of the Ocean
+          </CardSubtitle>
+          <CardText>
+            The Cosmic Clock Oyster House specializes in Avant-Garde seafood
+            dishes, from soups to salads, and of course, oysters.
+          </CardText>
+          <Button>Explore Cosmic Clock Oyster House</Button>
+        </CardBody>
+      </Card>
+    </>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Layout from "../components/Layout";
 import "@/styles/globals.css";
+import "../styles/bootstrap.min.css";
 
 export default function App({ Component, pageProps }) {
   return (
