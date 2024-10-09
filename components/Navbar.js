82aa2c159a1c0237aed3e3233335bdc2ca702822
archae13 <archae13@lpbroadband,net>
2024-10-09 11:31:49 -0600

    Navbar prepped

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,135 +1,52 @@
 import React, { useState, useContext } from 'react';
+
+import {useSession} from "next-auth/react";
 // @refresh reset
 
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText,
-} from 'reactstrap';
 
 function NavBar(args) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   const toggle = () => setIsOpen(!isOpen);
 
+  const { data: session, status } = useSession();
+
   return (
-    <div>
-      <Navbar {...args}>
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/cafes/">Cafes</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
-      </Navbar>
-    </div>
+
+
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">Cafe Dash</a>
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item">
+
+            {status === "authenticated" && <a className="nav-link" href="/cafes">Our Cafes</a>}
+            {status !== "authenticated" && <a className="nav-link disabled" href="/cafes">Our Cafes</a>}
+          
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a className="dropdown-item" href="#">Action</a>
+            <a className="dropdown-item" href="#">Another action</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <form className="form-inline my-2 my-lg-0">
+        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </nav>
   );
 }
 
 export default NavBar;
-
-/*
-
-const Navbar = () => {
-  const {user, setUser, isAuthenticated, setIsAuthenticated} = useContext(AppContext);
-
-  console.log("Navbar user:" ,user);
-  return (
-    <div className={styles.container}>
-      <div className={styles.item}>
-        <Image src="/img/logo.png" alt="logo" width={150} height={150} priority={true} style={{padding: "20px"}}/>
-        <ul className={styles.list}>
-          <li className={styles.listItem}>
-              <Link href="/">
-                Home
-              </Link>
-            </li>
-            <li className= {styles.listItem}>
-              {isAuthenticated ? (
-                <h5>{user.username}</h5>
-              ) : (
-                <Link href="/register">
-                  Sign up
-                </Link>
-              )}
-            </li>
-            <li className= {styles.listItem}>
-              {isAuthenticated ? (
-                <Link href="/" onClick={() => {
-                  logout();
-                  setUser(null);
-                }}>
-                
-                    Sign Out
-                  
-                </Link>
-              ) : (
-                <Link href="/api/auth/signIn">
-                  Sign in
-                </Link>
-              )}
-            </li>
-
-            <li className= {styles.listItem}>
-              {isAuthenticated ? (
-                <Link href="/cafes" onClick={() => {
-                  
-                }}>
-                
-                    Our Cafes
-                  
-                </Link>
-              ) : (
-                <></>
-              )}
-            </li>
-          </ul>
-        
-      </div>
-      <div className={styles.item}></div>
-      <div className={styles.callButton}>
-        <Image src="/img/telephone.png" alt="phone" width={32} height={32} />
-      </div>
-
-      <div className={styles.item}>
-        <div className={styles.cart}>
-          <Image src="/img/cart.png" alt="cart" width={30} height={30} />
-          <div className={styles.counter}>2</div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Navbar;
-
-*/
